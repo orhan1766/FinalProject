4d7f084821b14746b63d9403c13a7054f6003fb9
orhan1766@gmail.com <orhan1766@gmail.com>
2021-02-07 01:29:27 +0300

    Add project files.

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //generic constraint
+    //class: referans type(class demek değil)
+    //IEntity: IEntity olabilir veya IEntity'i implemente eden bir nesne olabilir
+    //new(): new'lenebilir nesne olmalıdır(IEntity olamaz çünkü interfacedir)
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //Context : DB tabloları ile proje classlarını bağlamak
+    public class NorthwindContext:DbContext
+    {
+        //senin projen hangi veritabanı ile ilişkili belirtilen yer
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=Northwind; Trusted_Connection=true");
+        }
+
+        //Benim Product nesnemi veri tabanındaki Products ile bağla (benim hangi classım hangi tabloya denk geliyor)
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
